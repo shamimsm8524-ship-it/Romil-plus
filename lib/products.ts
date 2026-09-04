40cf3ac8004ff61aaff7604dc9172486a16e1f6f
shamimsm8524-ship-it <jahan2307121025@diu.edu.bd>
2026-09-04 16:08:36 -0500

    Usa nueva imagen Autodesk en Romil Plus

--- lib/products.ts
+++ lib/products.ts
@@ -48,7 +48,7 @@ export const products: Product[] = [
     price: 10,
     duration: "12 meses",
     badge: "Promoción",
-    image: "/file_000000001ce0820ebf1be744d159107e.png",
+    image: "/autodesk-romil-plus.svg",
   },
   {
     id: "capcut-pro",
