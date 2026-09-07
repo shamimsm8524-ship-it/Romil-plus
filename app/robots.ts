8f67b7cf35420637234a174fe8e52d1f7f2fd89c
shamimsm8524-ship-it <jahan2307121025@diu.edu.bd>
2026-09-07 14:48:30 -0500

    Corregir robots para dominio www

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://romilplus.me/sitemap.xml",
-    host: "https://romilplus.me",
+    sitemap: "https://www.romilplus.me/sitemap.xml",
+    host: "https://www.romilplus.me",
   };
 }
